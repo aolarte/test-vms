6ed813738efa01f3931a85a22808b7dbd0658e7b
Andres Olarte <aolarte@grubhub.com>
2016-05-10 09:57:43 -0500

    kafka machine

---
+++ kafka/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# Setup puppet client
+if [ ! -f /etc/yum.repos.d/puppetlabs.repo ]; then
+	sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+fi
+
+if [ ! -f /usr/bin/puppet ]; then
+	sudo yum -y install puppet	
+fi
---
+++ kafka/manifests/default.pp
@@ -0,0 +1,72 @@
+file { "hosts":
+  path => "/etc/hosts",
+  source => '/vagrant/conf/hosts'
+}
+
+yumrepo { "cloudera-repo":
+  baseurl => "https://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/5/",
+  descr => "Cloudera's Distribution for Hadoop, Version 5",
+  enabled => 1,
+  gpgcheck => 1,
+  gpgkey =>  "https://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera"
+}
+
+
+yumrepo { "cloudera-kafka-repo":
+  require => [ Yumrepo["cloudera-repo"] ],
+  baseurl => "http://archive.cloudera.com/kafka/redhat/6/x86_64/kafka/2.0.1",
+  descr => "Cloudera's Distribution for Kafka",
+  enabled => 1,
+  gpgcheck => 1,
+  gpgkey =>  "http://archive.cloudera.com/kafka/redhat/6/x86_64/kafka/RPM-GPG-KEY-cloudera"
+}
+
+
+
+package { 'jre':
+  require => [ File["java_home.sh"] ],
+  name   => 'java-1.8.0-openjdk',
+  ensure => installed
+}
+
+file { "java_home.sh":
+  path => "/etc/profile.d/java_home.sh",
+  content => "export JAVA_HOME=/usr",
+  mode    => 755
+}
+
+package { 'zookeeper-server':
+  require => [ Yumrepo["cloudera-repo"], Package["jre"] ],
+  name   => 'zookeeper-server',
+  ensure => installed
+}
+
+package { 'kafka-server':
+  require => [ Yumrepo["cloudera-kafka-repo"], Package["zookeeper-server"] ],
+  name   => 'kafka-server',
+  ensure => installed
+}
+
+exec { "zookeeper-init":
+  require => [ Package['zookeeper-server'] ],
+  command => "/usr/bin/zookeeper-server-initialize",
+  user => "zookeeper",
+  creates => "/var/lib/zookeeper/version-2"
+}
+
+service { 'zookeeper-server':
+  require => [ Package['zookeeper-server'], Exec['zookeeper-init'] ],
+  ensure => running,
+}
+
+file { 'kafka-conf':
+  path => "/etc/kafka/conf/server.properties",
+  source => '/vagrant/conf/server.properties'
+}
+
+
+service { 'kafka-server':
+  require => [ Package['kafka-server'], Service['zookeeper-server'], File['kafka-conf'] ],
+  ensure => running,
+}
+
